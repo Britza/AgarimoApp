b0f4be5d48471c6439cc1d44b5ecba1fe19df6a0
britza <bzegarraflores@danielcastelao.org>
2022-03-01 14:07:56 +0100

    Hecha la parte grafica del inicio de la app y creada una constante para poder llevarla a las siguientes activitys

--- app/src/main/java/com/example/agarimoapp/InicioAgarimo.kt
+++ app/src/main/java/com/example/agarimoapp/InicioAgarimo.kt
@@ -1,11 +1,31 @@
 package com.example.agarimoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+const val EXTRA_MESSAGE = "com.dam2a.intent.MESSAGE"
 
 class InicioAgarimo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.inicio_agarimo)
+    }
+
+    fun profesional(View: View) {
+
+        val intent = Intent(this, Login::class.java).apply {
+            putExtra(EXTRA_MESSAGE, "profesional")
+        }
+        startActivity(intent)
+
+    }
+
+    fun paciente(View: View) {
+        val intent = Intent(this, Login::class.java).apply {
+            putExtra(EXTRA_MESSAGE, "paciente")
+        }
+        startActivity(intent)
     }
 }
